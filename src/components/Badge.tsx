d1e56dc57867046c88e68efb70808945544753ac
Julian Fernandez <fernandez_julian@hotmail.com>
2024-04-17 09:43:43 -0300

    Fix Badge component and branding issues in components

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,5 +1,3 @@
-import { onCleanup, onMount } from 'solid-js';
-
 type Props = {
   botContainer: HTMLDivElement | undefined;
   poweredByTextColor?: string;
@@ -9,33 +7,6 @@ type Props = {
 const defaultTextColor = '#303235';
 
 export const Badge = (props: Props) => {
-  let liteBadge: HTMLAnchorElement | undefined;
-  let observer: MutationObserver | undefined;
-
-  const appendBadgeIfNecessary = (mutations: MutationRecord[]) => {
-    mutations.forEach((mutation) => {
-      mutation.removedNodes.forEach((removedNode) => {
-        if ('id' in removedNode && liteBadge && removedNode.id == 'lite-badge') {
-          console.log("Sorry, you can't remove the brand 😅");
-          props.botContainer?.append(liteBadge);
-        }
-      });
-    });
-  };
-
-  onMount(() => {
-    if (!document || !props.botContainer) return;
-    observer = new MutationObserver(appendBadgeIfNecessary);
-    observer.observe(props.botContainer, {
-      subtree: false,
-      childList: true,
-    });
-  });
-
-  onCleanup(() => {
-    if (observer) observer.disconnect();
-  });
-
   return (
     <span
       class="w-full text-center px-[10px] pt-[6px] pb-[10px] m-auto text-[13px]"
